2092bf8f47f82e3a45b0e1f07ac7b5170dba1626
Jai12396 <74642321+Jai12396@users.noreply.github.com>
2021-06-05 18:13:07 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>React.js and Javascript Bootcamp!</h1>
+      <p>
+        The bootcamp on Javascript and React.js was just out of the world. It
+        was taken up by none other than Shaurya Sir. Everything was covered from
+        scratch(as Shaurya sir always mention there are no prerequisites)
+        including Javascript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
